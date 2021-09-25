3af1539ddd0aaff914506c200f32ab189ff6b010
LEdCF <prof.ferreira@yahoo.com.br>
2021-09-25 16:11:44 -0300

    Trilha Kotlin - Digital One

--- api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteArrayString.kt
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteArrayString.kt
@@ -2,9 +2,9 @@ package one.digitalinnovation.collections
 
 fun main() {
     val nomes = Array(3) {""}
-    nomes[0] = "Maria"
-    nomes[1] = "Zazá"
-    nomes[2] = "José"
+    nomes[0] = "Carla"
+    nomes[1] = "Luiz"
+    nomes[2] = "Eduardo"
 
     for (nome in nomes) {
         println(nome)
@@ -15,7 +15,7 @@ fun main() {
     nomes.forEach { println(it) }
 
     println("----------------------")
-    val nomes2 = arrayOf("Maria", "Zazá", "Pedro")
+    val nomes2 = arrayOf("Carla", "Luiz", "Pedro")
     nomes2.sort()
     nomes2.forEach { println(it) }
 }
